d9fd437637fdd36391ff0a0280ef824656a7477f
Darkvergil <yann.aquino@escolar.ifrn.edu.br>
2024-08-16 09:45:24 -0300

    Score feita

--- Trabas2d/Assets/Scripts/Orange.cs
+++ Trabas2d/Assets/Scripts/Orange.cs
@@ -8,17 +8,13 @@ public class Orange : MonoBehaviour
     private CircleCollider2D circle;
 
     public GameObject collected;
+    public int Score;
+    
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         circle = GetComponent<CircleCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -28,6 +24,9 @@ public class Orange : MonoBehaviour
             sr.enabled = false;
             circle.enabled = false;
             collected.SetActive(true);
+
+            GameController.instance.totalScore += Score;
+            GameController.instance.UpdateScoreText();
             
             Destroy(gameObject, 0.25f);
         }
