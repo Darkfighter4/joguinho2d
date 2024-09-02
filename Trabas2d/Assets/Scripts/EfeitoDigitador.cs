4a0cae12f8e3461eb5511c49372aa4a7dfe6b130
Darkvergil <yann.aquino@escolar.ifrn.edu.br>
2024-09-02 08:32:21 -0300

    Texto feito

--- Trabas2d/Assets/Scripts/EfeitoDigitador.cs
+++ Trabas2d/Assets/Scripts/EfeitoDigitador.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class EfeitoDigitador : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TextMeshProUGUI componenteTexto;
+    private AudioSource _audioSource;
+    private string mensagemOriginal;
+    public bool imprimindo;
+    public float tempoEntreLetras = 0.08f;
+
+    private void Awake()
     {
-        
+        TryGetComponent(out componenteTexto);
+        TryGetComponent(out _audioSource);
+        mensagemOriginal = componenteTexto.text;
+        componenteTexto.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        ImprimirMensagem(mensagemOriginal);
+    }
+
+    private void OnDisable()
+    {
+        componenteTexto.text = mensagemOriginal;
+        StopAllCoroutines();
+    }
+
+    public void ImprimirMensagem(string mensagem)
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            if(imprimindo) return;
+            imprimindo = true;
+            StartCoroutine(LetraPorLetra(mensagem));
+        }
+    }
+
+    IEnumerator LetraPorLetra(string mensagem)
+    {
+        string msg = "";
+        foreach (var letra in mensagem)
+        {
+            msg += letra;
+            componenteTexto.text = msg;
+            _audioSource.Play();
+            yield return new WaitForSeconds(tempoEntreLetras);
+        }
+
+        imprimindo = false;
+        StopAllCoroutines();
     }
 }
